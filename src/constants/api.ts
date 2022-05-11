38c0e02bac60a0ec89a89f576b65d0123ca044d0
Kolomiyets Ruslan <kolomiyetsruslan1@gmail.com>
2022-05-11 08:17:46 +0300

    fix deploy to github pages

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 export const WEATHER_API_KEY = 'd1cb55bfa0705e0ecb9ac27170a9a9d4';
 
-export const WEATHER_API_HOSTNAME = 'http://api.openweathermap.org';
+export const WEATHER_API_HOSTNAME = 'https://api.openweathermap.org';
 
 export const CURRENT_WEATHER_PATHNAME = 'data/2.5/weather';
 export const CITY_HISTORY_PATHNAME = 'data/2.5/onecall';
